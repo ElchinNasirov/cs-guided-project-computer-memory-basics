214cc1af42dc967ac57f6e39c7a4aa4f37e403e3
ElchinNasirov <nasirov.elchin@gmail.com>
2020-10-12 15:51:20 -0700

    Completed demonstration_3.py

--- src/demonstration_3.py
+++ src/demonstration_3.py
@@ -19,5 +19,25 @@ Input: arr = [1,1,2,2,2]
 Output: -1
 """
 def find_lucky(lst):
-    # Your code here
+    # UNDERSTAND
+    # if the count = value, return the value
+    # PLAN
+    # Define an output variable
+    output = -1
+    # Sort the list from largest to smallest
+    lst.sort(reverse=True)
+    # Make a set
+    set_list = set(lst)
+    # Iterate through the set, counting the # of times each value shows up
+    for number in set_list:
+        # Make a count variable
+        count = lst.count(number)
+        # Check if the count == the value
+        if number == count:
+            # If it does, add it to output variable
+            output = number
+    # If nothing matches, return -1
+    return output
 
+print(find_lucky([1,1,2,2,2]))
+print(find_lucky([1,2,2,3,3,3,4,4,4,4]))
